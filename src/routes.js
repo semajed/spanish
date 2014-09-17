2d82de90e344f26f8c2482deca8ed62a546a2c6a
James Dayhuff <dayhuffj@gmail.com>
2014-09-17 11:54:10 -0600

    ngroute now works!

--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,27 @@
-'use strict';
-var spanishClass = angular.module('spanishClass', ['ngRoute']);
-
-spanishClass.config(function ($routeProvider) {
+var spanishclass = angular.module('spanishclass',['ngRoute'])
+.config(function ($routeProvider) {
 	$routeProvider
 		.when('/home', {
-			templateUrl: 'partials/homepage.html'
-
+			templateUrl: 'views/homepage.html',
+			controller: 'mainController'
 		})
 		.when('/contactme', {
-			templateURL: 'partials/contactme.html'
-			
+			templateUrl: 'views/contactme.html',
+			controller: 'mainController'			
 		})
 		.when('/teacherinfo', {
-			templateURL: 'partials/teacher.html'
-			
-		})
-		.otherwise({
+			templateUrl: 'views/teacher.html',
+			controller: 'mainController'
+		}).when('/docsTEMPLATE', {
+			templateUrl: 'views/docsTEMPLATE.html',
+			controller: 'mainController'
+		}).when('/newsTEMPLATE', {
+			templateUrl: 'views/newsTEMPLATE.html',
+			controller: 'mainController'
+		}).when('/docsTEMPLATE', {
+			templateUrl: 'views/docsTEMPLATE.html',
+			controller: 'mainController'
+		}).otherwise({
 			template: "Page does not exist"
 		});
 });
-
-
-function mainCtrl ($scope) {
-    console.log('saysomething');
-}
-
-
-// app.config(function ($routeProvider){
-// 	$routeProvider
-// 		.when('/',{
-// 			templateUrl: "home.html"
-// 		})
-// 		.when('/home2',{
-// 			templateUrl: "home2.html",
-// 			controller: "employeeController"
-// 		})
-// 		.otherwise({
-// 			template: "This does not exist br0!"
-// 		});
-// });
